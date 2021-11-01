68cdeb1ca5c020216564b05b737b95a715a803c6
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-01 20:42:17 +0900

    ✨ Google Analyticsのスクリプトを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,17 @@ import '../styles/globals.css'
 import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "src/components/GoogleAnalytics";
+import usePageView from "src/hooks/usePageView";
 
 const MyApp = (props: AppProps) => {
-  return <props.Component {...props.pageProps} />;
+  usePageView();
+  return (
+    <>
+      <GoogleAnalytics />
+      <props.Component {...props.pageProps} />
+    </>
+  );
 };
 
 export default MyApp
