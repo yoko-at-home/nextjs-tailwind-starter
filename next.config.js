64b795c0c1fa52636e7231656252e5d522b66b4d
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-01 17:52:35 +0900

    しまぶーさんのeslint設定周りをコピー

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  rewrites: async () => {
+    return [{ source: "/", destination: "/root" }];
+  },
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
+  future: { strictPostcssConfiguration: true },
+  pageExtensions: [".tsx", ".ts", ".jsx", ".js"],
   reactStrictMode: true,
-}
+  poweredByHeader: false,
+};
+
+module.exports = {
+  nextConfig,
+  images: {
+    domains: ["images.unsplash.com", "source.unsplash.com", "unsplash.com", "images.microcms-assets.io"],
+  },
+};
