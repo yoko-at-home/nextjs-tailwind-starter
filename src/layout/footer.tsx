1ea9ff83d27e46f8211d66317451073d2fae31b8
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-05 16:48:16 +0900

    🚧 エラーになる箇所を非表示

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { CustomLink } from "src/components/CustomLink";
-import { SocialIcon } from "src/components/SocialIcons";
+// import { SocialIcon } from "src/components/SocialIcons";
 import { siteMetadata } from "src/data/siteMetadata";
 
 export const Footer = () => {
@@ -12,7 +12,7 @@ export const Footer = () => {
           <div className="flex mb-3 space-x-4">
             {/* <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} /> */}
             {/* <SocialIcon kind="github" href={siteMetadata.github} size={6} /> */}
-            <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
+            {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} /> */}
             {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
             {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} /> */}
           </div>
