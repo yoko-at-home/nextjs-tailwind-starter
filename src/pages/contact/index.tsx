376e5cb1bb06934d89783aa5cbf8c7e035a3ecc9
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-15 22:05:56 +0900

    レイアウト調整

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import type { NextPage } from "next";
 import { FormContact } from "src/components/FormContact";
 import { FixedLayout } from "src/layout";
@@ -5,7 +6,10 @@ import { FixedLayout } from "src/layout";
 const About: NextPage = () => {
   return (
     <FixedLayout theme="contact">
-      <FormContact />
+      お問い合わせ
+      <div className="flex justify-center">
+        <FormContact />
+      </div>
     </FixedLayout>
   );
 };
